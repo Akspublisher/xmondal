ee6bdf64bc2fde2502e07d238f7d7bfb7064c1da
hp <sudesh7678582882@gmail.com>
2026-02-10 19:27:56 +0530

    Add XMondal commit

---
+++ src/XMondal.js
@@ -0,0 +1,22 @@
+function XMondal() {
+    return (
+        <div style={{textAlign: "center"}}>
+            <h1 style={{margin: "20px"}}>User Details Mondal</h1>
+            <button style={{margin: "20px", 
+                background: "darkblue",
+                height:"40px", 
+                width:"150px",
+                borderRadius: "10px",
+                border: "none",
+                fontSize: "16px",
+                cursor: "pointer",
+                color: "white"
+                }}>
+                    Open Form</button>
+
+
+        </div>
+    )
+}   
+
+export default XMondal; 
